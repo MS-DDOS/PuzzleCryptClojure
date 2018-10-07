5a3384c75e21f2e95d226b21cc8f1bd01bb6481a
Tyler Springer <mspringer@smu.edu>
2018-10-06 17:09:54 -0700

    Finished first pass of encryption logic. Takes a file and splits into max n encrypted shards.

--- puzzle-crypt/src/puzzle_crypt/core.clj
+++ puzzle-crypt/src/puzzle_crypt/core.clj
@@ -5,14 +5,13 @@
   (require [lock-key.core :refer [decrypt decrypt-as-str decrypt-from-base64
                                    encrypt encrypt-as-base64]]))
 
-(def secret "hello world!")
-(def lock "password")
-(def encrypted-value (encrypt secret lock))
+(def byte-chunk-padding 0)
 
 (def cli-options
   [["-s" "--shards NUM" "The number of Shards you would like to split your file into."
     :default 1
-    :parse-fn #(Integer/parseInt %)]])
+    :parse-fn #(Integer/parseInt %)]
+   ["-k" "--key STRING" "The key to use for encrypting each shard."]])
 
 (defn byte-seq
   "Similar to https://clojuredocs.org/clojure.core/line-seq, but is byte-oriented instead of line-oriented.
@@ -24,26 +23,31 @@
         '()
         (cons ch (byte-seq rdr))))))
 
-(defn print-as-chunks
+(defn partition-byte-seq
+  "Partitions a byte-seq into a maximum of n sub-sequences."
   [input-byte-seq n]
-  (partition n n nil input-byte-seq))
+  (partition n n (repeat byte-chunk-padding) input-byte-seq))
+
+(defn encrypt-byte-chunks
+  "Encrypts a partitioned sequence of bytes."
+  [byte-chunk-coll key]
+  (let [byte-chunk-arrays (map #(into-array Byte/TYPE %) byte-chunk-coll)]
+    (map encrypt-as-base64 byte-chunk-arrays (repeat key))))
+
+(defn encrypt-file-as-chunks
+  "Shards a file into a given number of chunks and encrypts them."
+  [file-path number-of-shards encryption-key]
+  (let [file-length (.length (io/file file-path))
+        file-input-stream (byte-seq (io/reader file-path))
+        shard-size (long (Math/ceil (/ file-length number-of-shards)))
+        chunks (partition-byte-seq file-input-stream shard-size)]
+    (encrypt-byte-chunks chunks encryption-key)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [{:keys [options arguments errors summary]} (cli/parse-opts args cli-options)]
-    (let [file (io/file (first arguments))
-          file-input-stream (byte-seq (io/reader (first arguments)))
-          fileLength (.length file)
-          shards-needed (inc (long (/ fileLength (:shards options))))]
-      (println (print-as-chunks file-input-stream shards-needed))
-      (println "File length is:" fileLength)
-      (println "Shards needed is:" shards-needed))
-
-    (println args)
-    (println options)
-    (println arguments)
-    (println errors)
-    (println summary)
-    (println encrypted-value)
-    (println (decrypt encrypted-value lock))))
+    (assert (:key options) "`--key` must be specified")
+    (if errors
+      (println errors)
+      (println (encrypt-file-as-chunks (first arguments) (:shards options) (:key options))))))
